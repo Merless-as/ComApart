d7edfa3f65fb02a0fcda7e481db249b9cb49205e
Artem Samoilenko <>
2022-11-18 17:54:00 +0200

    bot.go updated

--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -1,6 +1,9 @@
 package telegram
 
-import "github.com/go-telegram-bot-api/telegram-bot-api"
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
 
 type Bot struct {
 	bot *tgbotapi.BotAPI
@@ -10,14 +13,38 @@ func NewBot(bot *tgbotapi.BotAPI) *Bot {
 	return &Bot{bot: bot}
 }
 
-func (b *Bot) Start() error{
+func (b *Bot) Start() error {
+	log.Println("Bot started")
+
+	updates, err := b.initUpdateChannel()
+	if err != nil {
+		return err
+	}
+
+	b.handlerUpdates(updates)
 	return nil
 }
 
 func (b *Bot) handlerUpdates(updates tgbotapi.UpdatesChannel) {
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
 
+		if update.Message.IsCommand() {
+			if err := b.handlerCommand(update.Message); err != nil {
+				continue
+			}
+		}
+		if err := b.handlerMessage(update.Message); err != nil {
+			continue
+		}
+	}
 }
 
-func (b *Bot) initUpdateChannel() error {
-	return nil
+func (b *Bot) initUpdateChannel() (tgbotapi.UpdatesChannel, error) {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	return b.bot.GetUpdatesChan(u)
 }
--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -6,19 +6,34 @@ func (b *Bot) handlerCommand(message *tgbotapi.Message) error {
 	switch message.Command() {
 	case "start":
 		return b.handlerStartCommand(message)
+	case "count":
+		return b.handlerCountCommand(message)
 	default:
 		return b.handlerUnknownCommand(message)
 	}
 }
 
 func (b *Bot) handlerMessage(message *tgbotapi.Message) error {
-	return nil
+	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
+
+	_, err := b.bot.Send(msg)
+	return err
 }
 
 func (b *Bot) handlerStartCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Приветствую, для начала работы введите /help")
+
+	_, err := b.bot.Send(msg)
+	return err
+}
+
+func (b *Bot) handlerCountCommand(message *tgbotapi.Message) error {
 	return nil
 }
 
 func (b *Bot) handlerUnknownCommand(message *tgbotapi.Message) error {
-	return nil
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Такая команда не зарегистрирована")
+
+	_, err := b.bot.Send(msg)
+	return err
 }
